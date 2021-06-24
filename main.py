618fde97b1cd691121966921dc03fea3cfcbd10a
Anirudh Iyer <anirudh.iyer@outlook.com>
2021-06-24 12:58:54 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import messagebox as mb
+
+ws = tk.Tk()
+ws.geometry('500x450+500+200')
+ws.title('To-Do List')
+ws.config(bg='#223441')
+ws.resizable(width=False, height=False)
+
+#
+
+
+# label for frame
+
+
+
+
+#################################################
+if __name__ == '__main__':
+    ws.mainloop()
